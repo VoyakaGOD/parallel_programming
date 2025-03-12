f48f06819d66cad2a49325890d4f46d2c687d9e8
VoyakaGOD <ab3000645@gmail.com>
2025-03-13 01:55:53 +0300

    decrease number of elements in exponent series

--- special_tasks/exp.cpp
+++ special_tasks/exp.cpp
@@ -1,18 +1,45 @@
 #include <big_decimal.hpp>
+#include <chrono>
+#include <cmath>
+
+uint32_t getInverseNlogN(uint32_t x)
+{
+    double low = 1;
+    double high = x;
+
+    while (high - low > 0.5)
+    {
+        double mid = (low + high) / 2;
+        double value = mid * std::log10(mid);
+
+        if (value < x)
+            low = mid;
+        else
+            high = mid;
+    }
+
+    return static_cast<uint32_t>((low + high) / 2);
+}
 
 int main(int argc, char** argv)
 {
     uint32_t precision = static_cast<uint32_t>(std::strtoul(argv[1], nullptr, 10));
     size_t fbc = (precision + BigDecimal::getBlockLength() - 1) / BigDecimal::getBlockLength();
     fbc += 2; // 2 additional blocks to account for the contribution of division remainders
+    size_t N = 2 * getInverseNlogN(precision + 1); // (N/2)lg(N/2) = lg(2) + precision
 
     BigDecimal e = BigDecimal::getOne(1, fbc, precision);
     BigDecimal item = BigDecimal::getOne(1, fbc, precision);
-    for(uint32_t n = 1; n < precision; n++)
+
+    auto start = std::chrono::system_clock::now();
+    for(uint32_t n = 1; n < N; n++)
     {
         item /= n;
         e += item;
     }
+    auto end = std::chrono::system_clock::now();
 
     std::cout << e << std::endl;
+    std::chrono::duration<double> time = end - start;
+    std::cout << "time: " << time.count() << " seconds" << std::endl;
 }
