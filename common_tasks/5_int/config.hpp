b449f32713e066f1f02e1dd7304facc38ae9fdb8
VoyakaGOD <ab3000645@gmail.com>
2025-04-21 20:09:59 +0300

    GOL: pthread measurements

--- common_tasks/5_int/config.hpp
+++ common_tasks/5_int/config.hpp
@@ -3,11 +3,11 @@
 
 #include <cmath>
 
-double epsilon = 1e-1;
-double from = 1e-4;
-double to = 1;
+double epsilon = 1e-6;
+double from = -2.99;
+double to = 5;
 
 double f(double x)
 {
-    return std::sin(1/x/x)/x/x;
+    return std::sin(1/(x + 3));
 }
